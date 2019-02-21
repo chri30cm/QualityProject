efbe1093286308903a827ab77f0abb5ceab9248d
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2019-02-21 12:47:21 +0100

    extra stuff

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -8,5 +8,26 @@ namespace Domain
 {
     public class Order
     {
+        private List<SaleOrderLine> orderlines = new List<SaleOrderLine>();
+        public Customer Customer { get; set; }
+        public int OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public string DeliveryDate { get; set; }
+        public bool Picked { get; set; }
+
+        public void AddOrderLine(Product product, int quantity, int price)
+        {
+
+        }
+
+        public void RemoveOrderLines(int index)
+        {
+
+        }
+
+        public List<SaleOrderLine> GetOrderLines()
+        {
+            return orderlines;
+        }
     }
 }
--- Domain/SaleOrderLine.cs
+++ Domain/SaleOrderLine.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    class SaleOrderLine
+    public class SaleOrderLine
     {
     }
 }
